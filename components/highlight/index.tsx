0f6122af12d0b6fee8e75310f52b6cd911d1f4c8
githaefrancis <mureithigithae@gmail.com>
2023-06-17 11:58:37 +0300

    feat(Arrows) - add arrow scroll

--- components/highlight/index.tsx
+++ components/highlight/index.tsx
@@ -1,6 +1,19 @@
+"use client";
+import { useRef } from "react";
 import styles from "./highlight.module.scss";
 import Image from "next/image";
 export default function Highlight() {
+  const scrollContainerRef = useRef(null);
+  const scrollAmount = 100;
+
+  const handleScroll = (event) => {
+    if (event.target.id === "scroll-left") {
+      scrollContainerRef.current.scrollLeft -= scrollAmount;
+    } else if (event.target.id === "scroll-right") {
+      scrollContainerRef.current.scrollLeft += scrollAmount;
+    }
+  };
+
   return (
     <div className={styles.highlight}>
       <h2 className={styles.heading2}>Product Center</h2>
@@ -11,8 +24,10 @@ export default function Highlight() {
           height={24}
           width={24}
           alt="left"
+          id="scroll-left"
+          onClick={handleScroll}
         />
-        <section className={styles.horizontalScroll}>
+        <section ref={scrollContainerRef} className={styles.horizontalScroll}>
           <div className={styles.card}></div>
           <div className={styles.card}></div>
           <div className={styles.card}></div>
@@ -35,6 +50,8 @@ export default function Highlight() {
           height={24}
           width={24}
           alt="right"
+          id="scroll-right"
+          onClick={handleScroll}
         />
       </section>
     </div>
