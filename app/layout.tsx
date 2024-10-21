9dc03a6a6a6c03915bd28c60f09947ec9af090f5
githaefrancis <mureithigithae@gmail.com>
2024-10-21 13:52:37 +0300

    remove old projects

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
       </Head>
       <UtilScript />
       <body>
-        <Banner />
+        {/* <Banner /> */}
         {/* <Header /> */}
         {children}
       </body>
